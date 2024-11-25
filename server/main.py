a3653cc734d43ab7698c5bf81bcff4120f8e3043
VarmaDev390 <raviteja.letcetra@gmail.com>
2024-11-25 11:35:20 +0530

    port change

--- server/main.py
+++ server/main.py
@@ -24,4 +24,6 @@ CORS(app, resources={r"/*": {"origins": allowed_origins}})
 if __name__ == "__main__":
     print("Connecting to MongoDB...")
     initialize_db()  # Initialize the DB
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
+   
